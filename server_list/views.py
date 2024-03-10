7f44569720635fe00d908109ae8f27a1dfdb087d
Isaac415 <isaac.cheng97@gmail.com>
2024-03-09 23:56:16 -0700

    Route all request to main page

--- server_list/views.py
+++ server_list/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from server_list.models import GameWorld
+from django.shortcuts import redirect
 
 # Create your views here.
+def servers(request):
+    gameworlds = GameWorld.objects.all()
+
+    return render(request, "server_list/server_list.html", {"gameworlds": gameworlds})
+
+def other(request):
+
+    return redirect('servers')
